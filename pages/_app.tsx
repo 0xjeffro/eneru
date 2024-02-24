cba270f5f52dc7980a9b819926a8bb1ee1622870
Jeffro Tanvik <00002@protonmail.com>
2024-02-24 15:13:25 +0800

    set dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Layout from "../components/mainNavi";
+import React, {useEffect, useState} from "react";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+
+    useEffect(() => {
+        const body = document.body;
+        body.setAttribute('theme-mode', 'dark');
+    });
+
+  return (
+      <Layout>
+      </Layout>
+      //<Component {...pageProps} />
+  );
 }
