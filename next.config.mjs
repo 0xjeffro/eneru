50a92e63e2ae0af8db403b3ba558e9c763f0a78e
Jeffro Tanvik <00002@protonmail.com>
2024-02-24 15:11:01 +0800

    use semi-ui in next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations', '@douyinfe/semi-icons-lab'],
 };
 
 export default nextConfig;
+
